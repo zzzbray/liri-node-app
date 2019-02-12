bd8956d639029ec527c2e753387ef8af7b6c2f5a
Cold Zebra <zachdbray@gmail.com>
2019-02-12 13:21:31 -0600

    built plan for code

--- liri.js
+++ liri.js
@@ -1,9 +1,27 @@
 require("dotenv").config();
-
+//Required vars
 var keys = require("./keys.js");
+var axios = require("axios");
+var fs = require("fs");
+
+//User vars
+var command = process.argv[2];
+var userQuery = process.argv.slice(3).join(" ");
+
 var spotify = new spotify(keys.spotify);
 
-concert-this
-spotify-this-song 
-movie-this
-dot-what-it-says 
+// concert-this
+// spotify-this-song 
+// movie-this
+// do-what-it-says 
+
+//Switch case that runs CLI app
+
+switch(command){
+  case "concert-this":
+  case "spotify-this-song":
+  case "movie-this":
+  case "do-what-it-says":
+  default:
+  console.log("Please choose from concert-this, spotify-this-song, movie-this or do-what-it-says");
+}
